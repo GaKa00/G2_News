8385b0e13148f60dbf32078dd858dccb440ef565
GaKa00 <145324695+GaKa00@users.noreply.github.com>
2024-01-29 09:12:17 +0100

    merged dev  into filter branch

--- G2news/Scripts/fetch.js
+++ G2news/Scripts/fetch.js
@@ -1,44 +1,12 @@
 let yesterday = new Date().getDate() - 1;
-const container = document.querySelector(".news-card-container");
-const searchBar = document.querySelector(".searchbar");
-const searchBtn = document.querySelector(".search-btn");
 const newAPI = "26873f2efaf240bfab3a9b7a0053b43b";
+const container = document.querySelector(".news-card-container");
+const searchBar = document.querySelector(".search-input");
+const searchBtn = document.querySelector(".search-btn");
 
-searchBtn.addEventListener("click ", function () {
-  let searchQuery = searchBar.value;
-  let search = searchQuery;
-  return search;
-});
-
-console.log(search);
-
-// create an array with the categories
-
-function checkFilters(query, category) {
-  if (!query && !category) {
-    //If neither category nor query is provided
-    const Link = `https://newsapi.org/v2/everything?domains=bbc.co.uk&from=${yesterday}&to=${yesterday}&apiKey=${newAPI}`;
-    return Link;
-  } else if (category) {
-    //If a category has been selected
-    const Link = `https://newsapi.org/v2/top-headlines/sources?&country=gb&category=${category}&apiKey=${newAPI}`;
-    return Link;
-  } else {
-    //if  a searchquery has been provided
-    const Link = `https://newsapi.org/v2/everything?q=${query}domains=bbc.co.uk&from=${yesterday}&to=${yesterday}&apiKey=${newAPI}`;
-    return Link;
-  }
-}
-
-//get query and category choice
-
-// `https://newsdata.io/api/1/news?apikey=${APIkey}`
-
-import axios from "axios";
-
-async function fetchData() {
+async function fetchData(query) {
   try {
-    const newsLink = checkFilters(search);
+    const newsLink = checkFilters(query);
     console.log(newsLink);
     const response = await axios.get(newsLink);
     console.log(response.data.articles);
@@ -59,6 +27,36 @@ async function fetchData() {
 }
 fetchData();
 
+searchBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  const searchQuery = searchBar.value;
+  fetchData(searchQuery);
+});
+
+// create an array with the categories
+
+function checkFilters(query, category) {
+  if (!query && !category) {
+    //If neither category nor query is provided
+    const Link = `https://newsapi.org/v2/everything?domains=bbc.co.uk&from=${yesterday}&to=${yesterday}&apiKey=${newAPI}`;
+    return Link;
+  } else if (category) {
+    //If a category has been selected
+    const Link = `https://newsapi.org/v2/top-headlines/sources?&country=gb&category=${category}&apiKey=${newAPI}`;
+    return Link;
+  } else {
+    //if  a searchquery has been provided
+    const Link = `https://newsapi.org/v2/everything?q=${query}&domains=bbc.co.uk&from=${yesterday}&to=${yesterday}&apiKey=${newAPI}`;
+    return Link;
+  }
+}
+
+//get query and category choice
+
+// `https://newsdata.io/api/1/news?apikey=${APIkey}`
+
+import axios from "axios";
+
 // `<div class="news-card">
 //         <img src="${articles.urlToImage}" alt="" />
 //         <h1 class="card-title">${articles.title}</h1>
@@ -76,3 +74,5 @@ Make a country bar?
 
 
 */
+
+//make search query look in object and display objects w same word in title
