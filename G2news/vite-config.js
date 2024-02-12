d2882b113d676dd3ebd0b4634870bbc774ba1697
GaKa00 <145324695+GaKa00@users.noreply.github.com>
2024-02-12 10:10:33 +0100

    Deploy

--- G2news/vite-config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  // Other Vite config options...
-  server: {
-    // Specify the directory containing your HTML files
-    publicDir: './'
-  }
-});
