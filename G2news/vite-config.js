7b8623fcf5089b30136e78f4652528a2b623a655
elinfelicia <elinriihimaki@gmail.com>
2024-02-15 13:15:38 +0100

    minor style fix

--- G2news/vite-config.js
+++ G2news/vite-config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Other Vite config options...
   server: {
-    // Specify the directory containing your HTML files
     publicDir: './'
   }
 });
