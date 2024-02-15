189fb168436584ad8939567da002054f74dce0a1
JohanTran <johan.tran0210@gmail.com>
2024-02-15 10:34:22 +0100

    css fixed

---
+++ G2news/public/main.js
@@ -0,0 +1,12 @@
+import './src/menu.js';
+
+import { fetchData } from './src/fetch.js';
+fetchData();
+
+import './src/favourite.js';
+
+import "./src/fetch.js";
+
+import './src/asideFetch.js'
+
+import './src/scroll.js'
